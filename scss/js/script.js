b3dfa1ea24418593266ea2e36ddd1da9f548c6d3
Emi <ebimatreshka3@gmail.com>
2022-11-15 01:24:23 +0900

    sp版menuクリックで表示まで完成

--- scss/js/script.js
+++ scss/js/script.js
@@ -1,6 +1,8 @@
-//ハンバーガーボタン
-$(".p-header__title--menu").on("click", function () {
-    $(this).toggleClass("is-open");
+//ハンバーガーボタン　クリックしたら黒の透過とsidebar表示
+$(function () {
+  $(".p-header__title--menu").on("click", function () {
+    $(".l-container__black").toggleClass("is-open");
     $(".l-sidebar").toggleClass("is-open");
-  });
-  
+    $(".l-body").toggleClass("is-open");
+  })
+})
