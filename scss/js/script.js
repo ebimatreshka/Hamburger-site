ff2c9d72abba50d9ebc1ad07409fbea28aba5161
Emi <ebimatreshka3@gmail.com>
2022-11-16 23:42:41 +0900

    sidebar崩れ修正

--- scss/js/script.js
+++ scss/js/script.js
@@ -1,8 +1,12 @@
 //ハンバーガーボタン　クリックしたら黒の透過とsidebar表示
 $(function () {
+  //menuをクリックしたら
   $(".p-header__title--menu").on("click", function () {
+    //黒透過幕が出てくる
     $(".l-container__black").toggleClass("is-open");
+    //sidebarが出てくる
     $(".l-sidebar").toggleClass("is-open");
+    //bodyのposition: fixed;を有効にする
     $(".l-body").toggleClass("is-open");
   })
 })
