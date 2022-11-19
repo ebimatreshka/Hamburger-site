1b0de949bb2b8a6562d4e08432c6a5469d8e9060
Emi <ebimatreshka3@gmail.com>
2022-11-19 13:13:58 +0900

    mobile版slideで表示まで

--- scss/js/script.js
+++ scss/js/script.js
@@ -1,29 +1,39 @@
-//headerのmenuをクリックしたらsidebarが出てくる
-$(".p-header__title--menu").click(function () { //ハンバーガーボタンをクリックしたら
-  $(this).toggleClass("active"); //それ自身にactiveをつけて
-  $(".l-sidebar").toggleClass("active"); //navにもactiveをつける
-});
+//ハンバーガーメニュー
+$(function () {
+  $(".p-header__title--menu").click(function () {
+    $(this).toggleClass("is-active");
+    $(".l-container__black").toggleClass("is-active"); //黒幕にis-activeをつける(表示)
+    $(".l-sidebar").toggleClass("is-open"); //sidebarにis-openをつける(表示)
+
+  })
+})
+
+
+// $(".p-header__title--menu").click(function () { //ハンバーガーボタンをクリックしたら
+//   $(this).toggleClass("active"); //それ自身にactiveをつけて
+//   $(".l-sidebar").toggleClass("active"); //navにもactiveをつける
+//   $(".l-container__black").toggleClass("active")
+// });
 
 
 
-// //ハンバーガーボタン　クリックしたら黒の透過とsidebar表示
-// $(function () {
-//   //menuをクリックしたら
-//   $(".p-header__title--menu").on("click", function () {
-//     //sidebarが出てくる
-//     $(".l-sidebar").toggleClass("is_active");
-//     //bodyのposition: fixed;を有効にする
-//     $(".l-body").toggleClass("is_active");
-//     //黒透過幕が出てくる
-//     $(".l-container__black").toggleClass("is-open");
-//   });
+//ハンバーガーボタン　クリックしたら黒の透過とsidebar表示
+  //menuをクリックしたら
+  // $(".p-header__title--menu").on("click", function () {
+  //   //sidebarが出てくる
+  //   $(".l-sidebar").toggleClass("is-active");
+  //   //bodyのposition: fixed;を有効にする
+  //   $(".l-body").toggleClass("is-active");
+  //   //黒透過幕が出てくる
+  //   $(".l-container__black").toggleClass("is-open");
+  // });
 
 
-//   $(".nav__hamburger-btn-wrapper--btn a").on("click",function () {
-//     // $(".p-header__title--menu").removeClass("is_active")
-//     // $(".l-sidebar").removeClass("is-active");
-//     $(".l-container__black").removeClass("is-open");
-//   });
+  $(".nav__hamburger-btn-wrapper--btn a").on("click",function () {
+    // $(".p-header__title--menu").removeClass("is-active")
+    // $(".l-sidebar").removeClass("is-active");
+    $(".l-container__black").removeClass("is-open");
+  });
 
   // $(".l-body").on("click",function () {
   //   $(this).removeClass("nav-open")
